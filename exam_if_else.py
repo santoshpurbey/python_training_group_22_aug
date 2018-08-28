edb9445c25a264a3fab8ef6110b24ce22db177e1
Santosh Purbey <santosh.purbey@logpoint.com>
2018-08-28 18:14:34 +0545

    exam marks example

--- exam_if_else.py
+++ exam_if_else.py
@@ -1,1 +1,16 @@
+mark = int(input('Enter your mark:'))
 
+if mark > 100:
+    print('Invalid Input, Please enter valid mark below 100')
+
+elif mark >= 80:
+    print('Congratulation! you got distionction')
+elif mark >= 65:
+    print('Congratulation! you got first divison ')
+elif mark >= 50:
+    print('Congratulation! you got second divison ')
+elif mark >= 40:
+    print('Congratulation! you got just pass')
+else:
+    print('Sorry you fail! better luck next time')
+    
